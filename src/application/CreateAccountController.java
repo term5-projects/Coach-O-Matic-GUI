80eba58b4a47c836a3efbcce939661091573315b
GracePearcey <pearcey.grace@gmail.com>
2023-03-31 16:13:14 -0230

    added a scene to create a new user, gives an alert if user info is not filled out

---
+++ src/application/CreateAccountController.java
@@ -0,0 +1,97 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+
+/**
+ * <h1>CreateAccountController</h1>
+ * Creates new user accounts.
+ * User must input a username and password to create the account.
+ * 
+ *
+ * @author Grace Pearcey
+ * @version 1.0
+ * @since 2023-03-31
+ */
+
+public class CreateAccountController {
+	
+	@FXML private Button saveAccountButton;
+	@FXML private Button returnButton;
+	
+	@FXML private TextField usernameTextField;
+	
+	@FXML private PasswordField passwordField;
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	/**
+	 * Creates a user and returns to login page if user enters something for both username and password fields.
+	 * Gives user an alert if either of the fields are empty.
+	 * 
+	 * 
+	 * @param event
+	 * @throws IOException
+	 */
+	public void createAccountAndExit(ActionEvent event) throws IOException {
+		boolean usernameEmpty = usernameTextField.getText().isBlank();
+		boolean passwordEmpty = passwordField.getText().isBlank();
+		
+		if (usernameEmpty == true || passwordEmpty == true) {
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setTitle("Create User Account");
+			alert.setHeaderText("Invalid Input");
+			alert.setContentText("Please create a username and password.");
+			
+			if (alert.showAndWait().get() == ButtonType.OK) {
+			}
+		}
+		else {
+			
+			//BE Connection TODO - Create User object with username and password
+		
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginScene.fxml"));
+			root = loader.load();
+					
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+	}
+	
+	/**
+	 * Returns user to login menu without creating a new user.
+	 * 
+	 * @param event
+	 * @throws IOException
+	 */
+	public void returnToLogin(ActionEvent event) throws IOException {
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginScene.fxml"));
+		root = loader.load();
+				
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	
+	}
+
+
+}
--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -26,6 +27,8 @@ import javafx.stage.Stage;
 */
 public class LoginController {
 
+		@FXML Button createAccountButton;
+		
 		@FXML TextField usernameTextField;
 		
 		@FXML PasswordField passwordField;
@@ -45,12 +48,27 @@ public class LoginController {
 			String username = usernameTextField.getText();
 			String password = passwordField.getText();
 			
+			//Check user credentials
+			
+			
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("UserMenuScene.fxml"));
 			root = loader.load();
 			
 			UserMenuController userMenuController = loader.getController();
 			userMenuController.displayName(username);
 			
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
+		
+		public void createAccount(ActionEvent event) throws IOException {
+
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("CreateAccountScene.fxml"));
+			root = loader.load();
+					
 			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 			scene = new Scene(root);
 			stage.setScene(scene);
