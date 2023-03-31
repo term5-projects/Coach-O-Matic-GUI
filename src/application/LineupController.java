9d88a62fbdc544e392e6ceb6a80f3fcf7a82636e
David Davila <adavidguamandavila@gmail.com>
2023-03-31 13:41:18 -0230

    Table View and Columns sample code implemented

--- src/application/LineupController.java
+++ src/application/LineupController.java
@@ -12,18 +12,41 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class LineupController{
+public class LineupController implements Initializable{
 	
-	@FXML Label formationLabel;
-	
+	@FXML private Label formationLabel;
 	
 	@FXML private AnchorPane LineupScenePane;
 	
 	@FXML private Button logoutButton;
+	
+	@FXML private TableView<SoccerPlayer> LineupTable;
+	
+	
+	@FXML private TableColumn<Person, String> shiftcolumn;
+	
+	@FXML private TableColumn<Person, String> GKcolumn;
+	
+	@FXML private TableColumn<Person, String> LDcolumn;
+	
+	@FXML private TableColumn<Person, String> RDcolumn;
+	
+	@FXML private TableColumn<Person, String> LMcoolumn;
+	
+	@FXML private TableColumn<Person, String> CMcolumn;
+	
+	@FXML private TableColumn<Person, String> RMcolumn;
+	
+	@FXML private TableColumn<Person, String> STcolumn;
+	
+	@FXML private TableColumn<Person, String> Subcolumn;
+	
 	
 	private Stage stage;
 	private Scene scene;
@@ -33,7 +56,7 @@ public class LineupController{
 	public void logout(ActionEvent event)
 
 	{
-
+Pl
 		Alert alert = new Alert(AlertType.CONFIRMATION);
 
 		alert.setTitle("Logout");
