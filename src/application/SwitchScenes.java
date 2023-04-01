994209fea13733d8bd2a089a5f86e607f04e0674
GracePearcey <pearcey.grace@gmail.com>
2023-04-01 12:28:56 -0230

    in progress

---
+++ src/application/SwitchScenes.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SwitchScenes {
+	
+	static void switchScene(FXMLLoader loader, Stage stage, Scene scene, Parent root, ActionEvent event) throws IOException {
+		root = loader.load();
+				
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+}
